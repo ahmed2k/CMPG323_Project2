6a2e239faa201a3b932a97a47b385af56efeab97
ahmed2k <a.hussein2k@gmail.com>
2021-11-01 10:49:02 +0200

    previously didnt have .net core installed now corrected

--- README.md
+++
@@ -1,2 +0,0 @@
-# CMPG323_Project2
-This is the repository for the photo album application
---
+++ photo album/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace photo_album.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
